58cbd97766f4ee65c01231b3237cfc7febc05fea
AmithSAI007 <amith@dataarize.com>
2026-03-21 00:48:36 +0530

    fix: add missing variable

--- terraform/modules/services/variables.tf
+++ terraform/modules/services/variables.tf
@@ -124,3 +124,8 @@ variable "service_account_name" {
   description = "The name of the service account to be used by the Cloud Run service."
   type        = string
 }
+
+variable "otel_exporter_otlp_headers" {
+  description = "A comma-separated list of key=value pairs to be included as headers in OpenTelemetry OTLP exporter requests."
+  type        = string
+}
